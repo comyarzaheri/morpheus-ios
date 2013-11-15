4702a570a6e72854f66945a53078af957b084d1f
Comyar <comyarzaheri@utexas.edu>
2013-11-15 16:54:20 -0600

    Added main view controller

---
+++ Morpheus/Morpheus/Controllers/MMRMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MMRMainViewController.h
+//  Morpheus
+//
+//  Created by Comyar Zaheri on 11/15/13.
+//  Copyright (c) 2013 Comyar Zaheri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMRMainViewController : UIViewController
+
+@end
---
+++ Morpheus/Morpheus/Controllers/MMRMainViewController.m
@@ -0,0 +1,21 @@
+//
+//  MMRMainViewController.m
+//  Morpheus
+//
+//  Created by Comyar Zaheri on 11/15/13.
+//  Copyright (c) 2013 Comyar Zaheri. All rights reserved.
+//
+
+#import "MMRMainViewController.h"
+
+@implementation MMRMainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
+        
+    }
+    return self;
+}
+
+@end
