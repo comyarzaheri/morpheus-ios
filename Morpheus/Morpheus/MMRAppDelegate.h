edaa475de59da51ba868197ff7c0cf9c95030e4b
Comyar <comyarzaheri@utexas.edu>
2013-11-15 16:41:19 -0600

    First Commit!

---
+++ Morpheus/Morpheus/MMRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MMRAppDelegate.h
+//  Morpheus
+//
+//  Created by Comyar Zaheri on 11/15/13.
+//  Copyright (c) 2013 Comyar Zaheri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Morpheus/Morpheus/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Morpheus
+//
+//  Created by Comyar Zaheri on 11/15/13.
+//  Copyright (c) 2013 Comyar Zaheri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MMRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MMRAppDelegate class]));
+    }
+}
